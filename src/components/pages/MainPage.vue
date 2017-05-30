7dcc47a010a063140861170d38820a8ca95f4507
Hansen Fahrezasandy <hansen.fahrezasandy@beauty-spotter.de>
2017-05-30 14:41:10 +0200

    Set up main page and add bulma and font awesome

---
+++ src/components/pages/MainPage.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="main-page">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainPage',
+  data () {
+    return {
+      msg: 'Main Page'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
